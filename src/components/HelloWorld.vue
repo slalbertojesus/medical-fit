660a922b7d4fea011d1538ff769234e897e51736
unknown <slalbertojesus@gmail.com>
2021-04-28 09:48:42 -0500

    Se añadió tamaños para citas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,51 +11,60 @@
             @click:date="displayDialog"
             @click:day="displayCitas"
           >
-            <template v-slot:day="{ date }">
-              <v-sheet
-                class="ma-4"
+            <template v-slot:day="{ date }"
+             v-if="calendario.length > 1">
+              <v-chip
                 v-model="citas"
                 v-for="cita in calendario[date]"
                 :key="cita.Nombre"
                 :title="category[0]"
                 color="primary"
                 rounded
-                height="16"
               >
-                <div class="chip-calendar">
-                  <v-row align="center" justify="center">
-                    <small class="white--text justify-center">
-                      {{ cita.Nombre }}
-                    </small>
-                    <v-icon small class="mx-1" color="white">
-                      mdi-calendar
-                    </v-icon>
-                  </v-row>
-                </div>
-              </v-sheet>
+                <small class="white--text justify-center">
+                  {{ cita.Nombre }}
+                </small>
+                <v-icon small class="mx-1" color="white">
+                  mdi-calendar
+                </v-icon>
+              </v-chip>
             </template>
           </v-calendar>
           <v-row>
             <v-col>
-              <h3>{{ currentDateTime() }}</h3>
+              <h3 class="mt-3">{{ currentDateTime() }}</h3>
             </v-col>
-            <v-col md="1">
-              <v-btn fab right dark color="red" @click="displayDialog">
+            <v-col class="text-right">
+              <v-btn
+                v-if="this.citasLista != 0"
+                fab
+                right
+                dark
+                color="red"
+                @click="displayDialog"
+              >
                 <v-icon dark>
                   mdi-plus
                 </v-icon>
               </v-btn>
             </v-col>
           </v-row>
+          <v-spacer></v-spacer>
           <v-row>
-            <v-col v-if="this.citasLista!= 0">
+            <v-col v-if="this.citasLista != 0">
               <h4 class="justify-center">Citas del día</h4>
               <CitasDia />
             </v-col>
-            <v-row justify="center" v-else>
-              <v-btn x-large outlined color="success" dark>
+            <v-row class="mt-2" justify="center" v-else>
+              <v-btn
+                x-large
+                outlined
+                color="success"
+                dark
+                @click="displayDialog"
+              >
                 Agregar una cita nueva
-                <v-icon>
+                <v-icon class="mx-2">
                   mdi-calendar-plus
                 </v-icon>
               </v-btn>
@@ -84,32 +93,7 @@ export default {
     citas: null,
     selectedDate: null,
     createEvent: null,
-    citasLista: [
-       {
-        fecha: "2021-04-09",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-        title: "Miguel Hernández",
-        hora: "10:00 AM",
-      },
-      {
-        fecha: "2021-04-10",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-        title: "Gibrán Posiot",
-        hora: "11:00 AM",
-      },
-      {
-        fecha: "2021-04-10",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-        title: "Monica Rivera",
-        hora: "2:00 PM",
-      },
-      {
-        fecha: "2021-04-11",
-        avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-        title: "Scarlet Calderón",
-        hora: "3:00 PM",
-      },
-    ],
+    citasLista: [],
     calendario: {
       "2021-04-09": [
         { Nombre: "Alberto de Jesús", Problema: "Dolor de gargante" },
